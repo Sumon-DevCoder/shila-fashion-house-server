b1780af452a2a9ceb49a1417f0791f5bdb090717
SumonRahman24 <mustafizurrahmansumon24@gmail.com>
2024-03-29 04:42:31 +0600

    mongodb server connected

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 4000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bwvmezk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -20,6 +21,40 @@ async function run() {
   try {
     // await client.connect();
     // await client.db("admin").command({ ping: 1 });
+
+    const reviewCollection = client.db("shilaFashionDB").collection("reviews");
+    const userCollection = client.db("shilaFashionDB").collection("users");
+    const productCollection = client
+      .db("shilaFashionDB")
+      .collection("products");
+
+    // products collecion apis route
+    app.get("/products", async (req, res) => {
+      const result = await productCollection.find().toArray();
+      res.send(result);
+    });
+
+    // review collection apis route
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
+
+    // user collection apis route
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+
+      // if user does exists
+      const query = { email: user?.email };
+      const isExists = await userCollection.findOne(query);
+      if (isExists) {
+        return res.send({ message: "email already exists", insertedId: null });
+      }
+
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
